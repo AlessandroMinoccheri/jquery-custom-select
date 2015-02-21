acad69de1c68778b538daf7bcb89155b9d90fb6d
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-02-21 20:08:06 +0100

    added first test in qunit

--- test/jquery.customselect.test.js
+++ test/jquery.customselect.test.js
@@ -1,5 +1,24 @@
 var QUNIT = true;
 $(document).ready(function(){
+    var mySelect = $('#my-select');
 
+    mySelect.customSelect({
+        width:100,
+        textAlign:"left",
+        textIndent:"10",
+        image:"select.jpg"
+    });
+  
+    module('Select new value');
+    test("Check Select option", function(){
+        numOptions = 0;
+        
+        mySelect.click();
+        numOptions = $(document).find('.select-ul').find('li').length;
+
+        console.log('numOptions: ' + numOptions);
+
+        notEqual( numOptions, '0');
+    });
 });
 
